f8e6fa73d00e9285fd9520a64113247866960fa8
MKMZ <rafal.klimek@student.uj.edu.pl>
2016-06-10 12:02:26 +0200

    FEATURE: implementing oauth for app IN PROGRESS

--- config/github.js
+++ config/github.js
@@ -3,14 +3,15 @@ const https = require('https');
 module.exports = (function(){
    
     return {
-        getReposList: getReposList
+        requestAuth: requestAuth,
+        getReposList: getReposList,
+        getReadme: getReadme
+        
     };
     
-});
+})();
 
 function getReposList(username, callback){
-    var that = this;
-    that.isWorking = true;
     var post_data = "";
     
     var options = {
@@ -22,7 +23,47 @@ function getReposList(username, callback){
       }
     };
     
-    sendRequest(post_data, options, function(response){
+    sendRequest(post_data, options, callback);
+
+}
+
+function getReadme(fullName, callback){
+    var post_data = "";
+    
+    var options = {
+      host: 'api.github.com',
+      path: '/repos/' + fullName + '/readme',
+      method: 'GET',
+      headers: {
+          'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'
+      }
+    };
+    
+    sendRequest(post_data, options, callback);    
+}
+
+function requestAuth(clientId, callback){
+//    var post_data = "client_id=" + clientId;
+    var post_data = "";
+    
+    var options = {
+      host: 'api.github.com',
+      path: '/login/oauth/authorize?client_id=' + clientId,
+      method: 'GET',
+      headers: {
+          'client_id'     : clientId,
+          'User-Agent'    : 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'
+//          'Content-Type'  : 'application/x-www-form-urlencoded',
+//          'Content-Length': post_data.length
+      }
+    };
+    
+    sendRequest(post_data, options, callback);       
+}
+
+
+function sendRequest(data, options, responseFunc, errorFunc){
+    var req = https.request(options, function(response){
 
         var resBody = '';
         response.on("data", function(chunk) {
@@ -30,15 +71,10 @@ function getReposList(username, callback){
         });
         response.on("end", function(){
             result = JSON.parse(resBody);
-            callback(result);
+            responseFunc(result);
             
         });
     });
-
-}
-
-function sendRequest(data, options, responseFunc, errorFunc){
-    var req = https.request(options, responseFunc);
     
     if(errorFunc !== undefined){
         req.on('error', errorFunc);
@@ -48,7 +84,7 @@ function sendRequest(data, options, responseFunc, errorFunc){
            console.log(err); 
         });
     }
-//    req.write(data);
+    req.write(data);
     req.end();
 }    
     
--- config/handler.js
+++ config/handler.js
@@ -1,19 +1,49 @@
-const async = require("async");
-
 const github = require("./github");
 
-module.exports = handler;
+module.exports = (function(){
+    init();
+    
+    return {
+        analysis: analysis
+    };
+    
+})(); 
 
-function handler(req, res){
+function init(){
+    github.requestAuth("f7959612467f83251b12", function(result){
+        console.log("Requested auth:");
+        console.log(result); 
+    });
+}
+
+function analysis(req, res){
     console.log("Starting handler");
     
-    var gh = new github();
-    gh.getReposList("MKMZ", function(result){
-        result.forEach(function(item){
-           console.log(item["full_name"]); 
-        });
+    
+    github.getReposList("Michsior14", function(result){
+        getReadmes(result);
         res.end(JSON.stringify(result));
+        
     });
     
+    
+}
+
+function getReadmes(reposList){
+   
+    for(var i = 0; i < reposList.length; i++){
+        github.getReadme(reposList[i]["full_name"], function(result){
+//            console.log("Next readme:");
+
+            if(result["content"] !== undefined){
+                console.log(result["content"]);
+                console.log(result["encoding"]);
+                var readmeText = Buffer.from(result["content"], "base64");
+                console.log(reposList[i]["full_name"] + ": ");
+                console.log(readmeText);
+            }
+
+        });
+    }
     
 }
