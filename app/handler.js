1a820d2e4af81d9da23bfa0c9da26fb16429b720
MKMZ <rafal.klimek@student.uj.edu.pl>
2016-06-23 13:14:10 +0200

    FIX: async algorythm to get all data

--- app/handler.js
+++ app/handler.js
@@ -1,4 +1,6 @@
-const github  =  require("./modules/github");
+const async = require('async');
+
+var github  =  require("./modules/github");
 const indeed  =  require("./modules/indeed");
 const dbpedia = require("./modules/dbpedia");
 const utilities = require("./utilities");
@@ -6,7 +8,7 @@ const utilities = require("./utilities");
 module.exports = (function(){
     var struct = {
         analysis: analysis,
-        isReady: false
+        token: null
     };
 
     init(struct);
@@ -17,67 +19,119 @@ module.exports = (function(){
 
 function init(self){
 
-//    indeed.searchJobs("software developer", "Nijmegen", function(result, request){
-//        console.log(result);
-//        console.log(request);
-//    });
+  //  dbpedia.requestSpotlight("Linux is a clone of the operating system Unix, written from scratch by" +
+  //"Linus Torvalds with assistance from a loosely-knit team of hackers across" +
+  //"the Net. It aims towards POSIX and Single UNIX Specification compliance." +
+  //"It has all the features you would expect in a modern fully-fledged Unix," +
+  //"including true multitasking, virtual memory, shared libraries, demand "+
+  //"loading, shared copy-on-write executables, proper memory management, "+
+  //"and multistack networking including IPv4 and IPv6. " +
+  //"It is distributed under the GNU General Public License - see the "+
+  //"accompanying COPYING file for more details.", 0.35, function(result, response){
+  //      var dbptf = [];
+		//utilities.dbpediaTF(result, dbptf);
+		//utilities.normalizeTF(dbptf);
+		//var test = [];
+		//test.push(dbptf);
+		//testcount = [];
+		//testcount.push({ term : 'Unix', count : 2});
+		//testcount.push({ term : 'Linux', count: 1});
+		//testcount.push({ term : 'Test', count: 2});
+		//utilities.normalizeTF(testcount);
+		//test.push(testcount);
+		//testcount2 = [];
+		//testcount2.push({ term : 'Unix', count: 5});
+		//testcount2.push({ term : 'Testing', count : 1});
+		//utilities.normalizeTF(testcount2);
+		//test.push(testcount2);
+		//query = [];
+		//query.push({ term : 'Unix', count: 5});
+		//query.push({ term : 'Test', count: 5});
+		//utilities.normalizeTF(query);
+		//console.log(utilities.findBestMatch(query, test));
+  //  });
 
-    dbpedia.requestSpotlight("Linux is a clone of the operating system Unix, written from scratch by" +
-  "Linus Torvalds with assistance from a loosely-knit team of hackers across" +
-  "the Net. It aims towards POSIX and Single UNIX Specification compliance." +
-  "It has all the features you would expect in a modern fully-fledged Unix," +
-  "including true multitasking, virtual memory, shared libraries, demand "+
-  "loading, shared copy-on-write executables, proper memory management, "+
-  "and multistack networking including IPv4 and IPv6. " +
-  "It is distributed under the GNU General Public License - see the"+
-  "accompanying COPYING file for more details.", 0.35, function(result, response){
-        var dbptf = [];
-		utilities.dbpediaTF(result, dbptf);
-		utilities.normalizeTF(dbptf);
-		var test = [];
-		test.push(dbptf);
-		testcount = [];
-		testcount.push({ term : 'Unix', count : 2});
-		testcount.push({ term : 'Linux', count: 1});
-		testcount.push({ term : 'Test', count: 2});
-		utilities.normalizeTF(testcount);
-		test.push(testcount);
-		testcount2 = [];
-		testcount2.push({ term : 'Unix', count: 5});
-		testcount2.push({ term : 'Testing', count : 1});
-		utilities.normalizeTF(testcount2);
-		test.push(testcount2);
-		query = [];
-		query.push({ term : 'Unix', count: 5});
-		query.push({ term : 'Test', count: 5});
-		utilities.normalizeTF(query);
-		console.log(utilities.findBestMatch(query, test));
-    });
+    github.getAuth(function(result){
+        if (result["token"]) {
+            github.token = result["token"];
+            console.log(" Application successfully authenticated on GitHub API");
+        }
+        else {
+            console.log(" An error occured during authentication on GitHub API");
+        }
+       
+       
+   });
 
-//    github.getAuth(function(result){
-////       console.log(result);
-//       github.token = result["token"];
-//       self.isReady = true;
-//    });
 
-	//indeed.prepareJobs("Amsterdam", "nl", function (jobTable) {
-	//	console.log("jobTable: ");
-	//	console.log(jobTable);
-	//});
 
 }
 
 function analysis(req, res){
+    
 
-    github.getReposList("Michsior14", function(result){
-        getReadmes(result);
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(JSON.stringify(result));
+    //github.getReposList("Michsior14", function (result) {
+    //    getReadmes(result);
+    //    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //    res.end(JSON.stringify(result));
 
-    });
+    //});
+    level1("Michsior14", "Amsterdam", "nl");
 
 
 }
+
+function level1(username, city, country){
+    
+    var counter = 0;
+    var userText = "";
+    
+    async.parallel([
+        function (callback){
+            github.getReposList(username, function (reposList) {
+                
+                async.forEachOf(reposList, function (item1, idx1, callback1) {
+                    github.getReadme(item1["full_name"], function (result, request) {
+                        if (result["content"] !== undefined) {
+                            var readmeText = new Buffer(result["content"], result["encoding"]).toString();
+                            
+                            userText += readmeText + " ";     
+                        }
+                        callback1();
+                    });
+                }, function (err1) {
+                    if (err1) {
+                        console.error(err1.message);
+                    }
+                    //console.log("userText:");
+                    //console.log(userText);
+                    dbpedia.requestSpotlight(userText, 0.35, function (result, request) {
+                        
+                    });
+
+                    callback();	                    
+                });
+
+            });
+        }, function (callback){
+            indeed.prepareJobs(city, country, function (jobTable) {
+                
+                               
+                callback();
+            });
+
+        }
+    ], function (err) {
+        if (err) {
+            console.error(err.message);
+        }    
+    
+    });
+
+
+
+}
+
 
 function getReadmes(reposList){
 
