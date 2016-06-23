8bee8a71750b58de9f9ed805dd1e6c23f12b2fce
Aga Rakowska <ag.rakowska@gmail.com>
2016-06-24 00:35:52 +0200

    dbpediaTF should count more complex phrases

--- test/dbpediaTest.js
+++ test/dbpediaTest.js
@@ -19,24 +19,40 @@ describe('Test Suite DBPedia', function () {
             expect(result).to.not.have.property('asdasdasdasdasdads');
         });
     });
-});
 
-describe('dbpediaTF', function () {
     it('dbpediaTF should count all terms found by dbpedia', function () {
         dbpedia.requestSpotlight("Linux Unix Posix there are no terms here Hackers Unit Test Linux Linux Linux", 0.35, function (result, response) {
             var tf = utilities.dbpediaTF(result);
+            expect(tf).to.be.an('array');
+            expect(tf[0]).to.have.property('term');
+            expect(tf[0]).to.have.property('count');
+
             expect(tf.length).to.equal(5);
             expect(tf[0].count).to.equal(4);
         });
     });
-});
 
-describe('dbpediaTF', function () {
     it('dbpediaTF should count more complex phrases', function () {
         dbpedia.requestSpotlight("World of Warcraft asdasdsda Solaris asdasd World of a Warcraft asdad Solaris asdasd Solaris", 0.35, function (result, response) {
             var tf = utilities.dbpediaTF(result);
             expect(tf.length).to.equal(2);
-            expect(tf[0].count).to.equal(7);
+            var flag = false;
+            for (var i = 0; i < tf.length; i++) {
+                if (tf[i].term == "Warcraft") {
+                    flag = true;
+                    break;
+                }
+            }
+            expect(flag).to.equal(true);
+            
+            flag = false;
+            for (var i = 0; i < tf.length; i++) {
+                if (tf[i].term == "Solaris") {
+                    flag = true;
+                    break;
+                }
+            }
+            expect(flag).to.equal(true);
         });
     });
 });
---
+++ test/serverTest.js
@@ -0,0 +1,10 @@
+﻿const chai = require('chai');
+const expect = chai.expect;
+
+
+describe('Test Server', function () {
+    it('check the result of main request', function () {
+        assert.ok(true, "This shouldn't fail");
+    });
+
+});
