ce21ac5aca9b832a3130938de4bfa5ecd6552094
Aga Rakowska <ag.rakowska@gmail.com>
2016-06-24 13:52:07 +0200

    FIX: test server

--- test/serverTest.js
+++ test/serverTest.js
@@ -7,7 +7,7 @@ const expect = chai.expect;
 
 describe('Test Server', function () {
     it('check the result of main request', function () {
-        chai.request('http://localhost:3000')
+        chai.request('http://localhost:3000/service?username=Michsior14&city=Warsaw&country=pl')
         .get('/')
         .end(function (err, res) {
             expect(err).to.be.null;
